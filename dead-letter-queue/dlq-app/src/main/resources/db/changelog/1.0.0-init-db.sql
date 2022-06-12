42fcccf68631727effd8df00fe9491540b89ae64
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2022-06-12 22:33:47 +0200

    Add database initialization script and add active initialization in the dql-app module using liquibase

---
+++ dead-letter-queue/dlq-app/src/main/resources/db/changelog/1.0.0-init-db.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset frmi:1
+
+CREATE SEQUENCE IF NOT EXISTS dlq_record_sequence
+    INCREMENT 50
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE TABLE IF NOT EXISTS dlq_record
+(
+    id bigint NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    dequeued boolean NOT NULL,
+    exception oid,
+    message oid NOT NULL,
+    CONSTRAINT dlq_record_pkey PRIMARY KEY (id)
+)
+
+-- ROLLBACK DROP SEQUENCE IF EXISTS dlq_record_sequence;
+-- ROLLBACK DROP TABLE IF EXISTS dlq_record;
