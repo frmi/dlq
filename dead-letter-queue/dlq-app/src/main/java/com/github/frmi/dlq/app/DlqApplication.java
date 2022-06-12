7db9f5d61a51feab8416fef465159c64a39dc78c
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2022-06-12 22:39:15 +0200

    Remove @EnableDeadLetterQueue from DqlApplication.java. This has been moved to DqlConfig.java

--- dead-letter-queue/dlq-app/src/main/java/com/github/frmi/dlq/app/DlqApplication.java
+++ dead-letter-queue/dlq-app/src/main/java/com/github/frmi/dlq/app/DlqApplication.java
@@ -1,10 +1,8 @@
 package com.github.frmi.dlq.app;
 
-import com.github.frmi.dlq.api.EnableDeadLetterQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableDeadLetterQueue
 @SpringBootApplication
 public class DlqApplication {
 
