bf5713d85a6ecc059f00ede3f5330bc255f28554
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2022-06-12 22:36:19 +0200

    Fix typo

--- dead-letter-queue/dlq-api/src/main/java/com/github/frmi/dlq/api/web/error/DeadLetterQueueAdvice.java
+++ dead-letter-queue/dlq-api/src/main/java/com/github/frmi/dlq/api/web/error/DeadLetterQueueAdvice.java
@@ -1,32 +1,29 @@
 package com.github.frmi.dlq.api.web.error;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class DeadLetterQueueAdvice {
 
     @ResponseBody
     @ExceptionHandler(DlqRecordNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     String recordNotFoundHandler(DlqRecordNotFoundException ex) {
         return ex.getMessage();
     }
 
     @ResponseBody
     @ExceptionHandler(DlqRetryFailedException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     String retryFailedHandler(DlqRetryFailedException ex) {
         return ex.getMessage();
     }
 
     @ResponseBody
-    @ExceptionHandler(DlQRecordAlreadyRetriedException.class)
+    @ExceptionHandler(DlqRecordAlreadyRetriedException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    String alreadyRetriedHandler(DlQRecordAlreadyRetriedException ex) {
+    String alreadyRetriedHandler(DlqRecordAlreadyRetriedException ex) {
         return ex.getMessage();
     }
 
